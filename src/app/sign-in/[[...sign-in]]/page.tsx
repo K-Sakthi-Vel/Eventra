3383f8a9a7cd395fea1676d907315825a541b7aa
K SAKTHIVEL <k.sakthivel@revca.tech>
2025-08-03 22:26:06 +0530

    responsiveness update

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -6,12 +6,15 @@ export default function SignInPage() {
   return (
     <ClerkProvider>
       <div className="font-sans antialiased min-h-screen bg-gray-100 h-full">
-        <main className="h-[100vh] flex items-center justify-center px-4 py-12">
-          <div className="w-full max-w-md rounded-xl bg-white p-8">
+        <main className="h-[100vh] flex items-center justify-center px-4">
+          <div className="flex flex-col items-center w-full max-w-md rounded-xl bg-white py-5">
             <h1 className="mb-6 text-2xl font-bold text-center text-gray-800">
               Welcome Back
             </h1>
+            <div>
             <SignIn/>
+
+            </div>
             <div className="text-center mt-4">
               <span className="text-gray-600">Don’t have an account?</span>{' '}
               <SignUpButton>
